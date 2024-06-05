7c75c3ed693146bad24d4d0bbc6603afc6d77995
mamatapoudel777 <131430891+mamatapoudel777@users.noreply.github.com>
2024-06-06 00:24:48 +0545

    Revert "Mamata"

--- newguest.vb
+++ newguest.vb
@@ -1,12 +1,8 @@
 ﻿Imports System.Data.SqlClient
-<<<<<<< HEAD
-Public Class Newguestform
-=======
 
 
 
 Public Class Newguest_form
->>>>>>> master
     'Dim con As New sqlconnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\uSer\OneDrive\Documents\CordialSuiteDB.mdf;Integrated Security=True;Connect Timeout=30")
     Private Sub Newguestbtn_Click(sender As Object, e As EventArgs) Handles addnewguest_btn.Click
 
@@ -15,18 +11,6 @@ Public Class Newguest_form
     Private Sub Closebtn_Click(sender As Object, e As EventArgs) Handles closebtn.Click
         Me.Dispose()
         End
-
-    End Sub
-
-    Private Sub gfirstnametxtbx_TextChanged(sender As Object, e As EventArgs) Handles gfirstnametxtbx.TextChanged
-
-    End Sub
-
-    Private Sub glastnametxtbx_TextChanged(sender As Object, e As EventArgs) Handles glastnametxtbx.TextChanged
-
-    End Sub
-
-    Private Sub combobxgender_SelectedIndexChanged(sender As Object, e As EventArgs) Handles combobxgender.SelectedIndexChanged
 
     End Sub
 End Class
